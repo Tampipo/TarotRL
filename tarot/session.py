98e59d4334a9ecb39e7e48115a4651e268f5d71c
Tanguy <tanguy.marsault02@gmail.com>
2026-01-04 20:02:04 +0100

    Fix the score updating between games and score computing overall

--- tarot/session.py
+++ tarot/session.py
@@ -11,6 +11,7 @@ class TarotSession:
         self.current_game = 0
         self.games = []
         self.scores = {player.name: 0 for player in players}
+        self.N_players = len(players)
         print(f"Starting Tarot session with {ngames} games and players: {[player.name for player in players]}")
 
     def update_deck(self):
@@ -28,12 +29,45 @@ class TarotSession:
     
     def play_game(self):
         new_game = TarotGame(self.players, self.deck, dealer=self.dealer)
-        final_score = new_game.play_game()
-        if final_score:
-            for player in new_game.team1:
-                self.scores[player.name] += final_score
-            for player in new_game.team2:
-                self.scores[player.name] -= final_score
+        new_game.play_game()
+        final_score = new_game.final_score
+        if final_score == 0:
+            with open("test.txt", "w") as f:
+                f.write(str(final_score))
+        
+
+        if new_game.taker is None:
+            return  # No score update if no taker
+
+        if self.N_players == 3:
+            # The taker wins twice as much
+            self.scores[new_game.taker.name] += final_score * 2
+            for player in self.players:
+                if player.name != new_game.taker.name:
+                    self.scores[player.name] -= final_score
+        if self.N_players == 4:
+            # The taker wins three times as much
+            self.scores[new_game.taker.name] += final_score * 3
+            for player in self.players:
+                if player.name != new_game.taker.name:
+                    self.scores[player.name] -= final_score
+        if self.N_players == 5:
+            # Two cases, if the taker is alone he wins 4 times as much, if he is with a teammate the team mate wins 1, he wins 2
+            if len(new_game.team1.players) == 1:
+                self.scores[new_game.taker.name] += final_score * 4
+                for player in self.players:
+                    if player.name != new_game.taker.name:
+                        self.scores[player.name] -= final_score
+            else:
+                for player in new_game.team1.players:
+                    if player.name == new_game.taker.name:
+                        self.scores[player.name] += final_score * 2
+                    else:
+                        self.scores[player.name] += final_score
+                for player in new_game.team2.players:
+                    self.scores[player.name] -= final_score
+
+            
 
         self.games.append(new_game)
         self.current_game += 1
